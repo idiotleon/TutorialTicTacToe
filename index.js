708fac43c633a13ad76adf2170b7bc9c0a29331c
Yang Lu <leon@Yangs-MBP.home>
2018-03-08 09:43:31 -0800

    fea: passing data through props

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class Square extends React.Component{
     render(){
         return(
             <button className="square">
-                {/* TODO */}
+                {this.props.value}
             </button>
         );
     }
@@ -34,7 +34,7 @@ class Square extends React.Component{
 
 class Board extends React.Component{
     renderSquare(i){
-        return <Square />;
+        return <Square value={i} />;
     }
 
     render(){
@@ -79,7 +79,7 @@ class Game extends React.Component{
     }
 }
 
-// =============================================
+// ================================================
 
 ReactDOM.render(
     <Game />,
