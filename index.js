d928f4d366201611bf2de98447440bd0cebc9944
Yang Lu <leon@Yangs-MBP.home>
2018-03-08 09:47:14 -0800

    demo: arrow function syntax

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ class ShoppingList extends React.Component{
 class Square extends React.Component{
     render(){
         return(
-            <button className="square">
+            <button className="square" onClick={()=>alert('click')}>
                 {this.props.value}
             </button>
         );
