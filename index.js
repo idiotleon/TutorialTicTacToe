eb06c44d080bd3041c4fa4e0ebfbef4fa50a9944
Yang Lu <leon@Yangs-MBP.home>
2018-03-11 23:47:35 -0700

    demo example: An Interative Component

--- index.js
+++ index.js
@@ -23,10 +23,33 @@ class ShoppingList extends React.Component{
 // Example usage: <ShoppingList name="Mark" />
 
 class Square extends React.Component{
+    /*  React components can have state by setting this.state
+    *   in the constructor, which should be considered private 
+    *   to the component.
+    */
+    constructor(props){
+        /*  In JavaScript classes, you need to
+        *   explicitly call super(), when defining
+        *   the constructor of a subclass
+        */
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+    
     render(){
         return(
-            <button className="square" onClick={()=>alert('click')}>
-                {this.props.value}
+            /*
+            *   Whenver this.setState is called, an update to
+            *   the component is scheduled, causing React to merge
+            *   in the passed state update and rerender the component
+            *   along with its dependencies. When the component rerenders,
+            *   'this.state.value' will be 'X' so you'll see X in the grid
+            */
+            <button className="square" onClick={() =>
+            this.setState({value:'X'})}>
+                {this.state.value}
             </button>
         );
     }
